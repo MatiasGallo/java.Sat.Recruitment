bbacefeb867859399b2d7a971041c4e8e81b3652
matias-gallo <matias.a.gallo.96@gmail.com>
2022-05-31 23:32:16 -0300

    Service Test

--- src/test/java/platform/messagingservice/api/service/ServiceTest.java
+++ src/test/java/platform/messagingservice/api/service/ServiceTest.java
@@ -1,11 +1,44 @@
 package platform.messagingservice.api.service;
 
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import sat.recruitment.api.SatRecruitmentApplication;
+import sat.recruitment.api.service.SatRecruitmentService;
+import sat.recruitment.model.user.User;
+import sat.recruitment.model.user.UserBuilder;
+
+@SpringBootTest(classes = SatRecruitmentApplication.class)
 public class ServiceTest {
+	@Autowired
+    private SatRecruitmentService service = new SatRecruitmentService();
+    
+    UserBuilder builder = new UserBuilder();
 	
-	@Test
-    public void main() {
+    @Test
+    void testCreateDuplicateUserMail() {
+    	User newUser = builder.createdUser("Raul", "Franco.Perez@gmail.com", "Colombia 115", "+534555213454", "Premium", 200.0);
+    	assertThat(service.isUserDuplicated(newUser)).isEqualTo(true);
+    }
+    
+    @Test
+    void testCreateDuplicateUserPhone() {
+    	User newUser = builder.createdUser("Raul", "Raul@gmail.com", "Colombia 115", "+5491154762312", "Premium", 200.0);
+    	assertThat(service.isUserDuplicated(newUser)).isEqualTo(true);
+    }
+    
+    
+    @Test
+    void testCreateDuplicateUserNameAdress() {
+    	User newUser = builder.createdUser("Agustina", "Raul@gmail.com", "Garay y Otra Calle", "+534645213513", "Premium", 200.0);
+    	assertThat(service.isUserDuplicated(newUser)).isEqualTo(true);
     }
 
+    @Test
+    void testCreateUniqueUser() {
+    	User newUser = builder.createdUser("Agustina", "Raul@gmail.com", "Brasil 111", "+534645213513", "Premium", 200.0);
+    	assertThat(service.isUserDuplicated(newUser)).isEqualTo(false);
+    }
 }
