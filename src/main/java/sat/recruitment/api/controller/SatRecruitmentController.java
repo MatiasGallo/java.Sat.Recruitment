ecaa05fc75905bb6be50ab9bbdfc68cee85cee41
matias-gallo <matias.a.gallo.96@gmail.com>
2022-06-01 22:11:48 -0300

    Added simple loggin

--- src/main/java/sat/recruitment/api/controller/SatRecruitmentController.java
+++ src/main/java/sat/recruitment/api/controller/SatRecruitmentController.java
@@ -2,6 +2,7 @@ package sat.recruitment.api.controller;
 
 import javax.validation.Valid;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -16,16 +17,20 @@ import sat.recruitment.api.service.SatRecruitmentService;
 import sat.recruitment.model.user.User;
 import sat.recruitment.model.user.UserBuilder;
 import sat.recruitment.model.user.UserDTO;
+import org.slf4j.Logger;
 
 @RestController
 @RequestMapping(value = "/api/v1")
 public class SatRecruitmentController {
 	@Autowired
 	private SatRecruitmentService userService;
-
+	
+	Logger logger = LoggerFactory.getLogger(SatRecruitmentController.class);
+	
 	@PostMapping(value = "/create-user", consumes = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseStatus(value = HttpStatus.CREATED)
 	public ResponseEntity<String> createUser(@Valid @RequestBody UserDTO messageBody) {	
+		logger.info("Starting createUser");
 		
 		UserBuilder builder = new UserBuilder();
 		User newUser = builder.createdUser(messageBody.getName(), messageBody.getEmail(), messageBody.getAddress(), messageBody.getPhone(), messageBody.getUserType(),messageBody.getMoney());
@@ -34,7 +39,8 @@ public class SatRecruitmentController {
 		if (userService.isUserDuplicated(newUser)) {
 			return new ResponseEntity<String>("User duplicated",HttpStatus.BAD_REQUEST);
 		}
-
+		
+		logger.info("Finishing createUser");
 		return new ResponseEntity<String>("User created", HttpStatus.CREATED);
 	}
 }
